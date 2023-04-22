e159a90ba29c3a768bda3a7f787f392d37fcdb05
Jerry Teng <zuyuangteng@gmail.com>
2023-04-22 12:04:01 -0500

    allow requests from front end

--- expressExample/app.js
+++ expressExample/app.js
@@ -6,7 +6,7 @@ const db = require("./models");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 const mysql = require('mysql2');
